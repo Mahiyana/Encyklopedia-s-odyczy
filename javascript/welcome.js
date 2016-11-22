6e849d6fc89fae2fa983a5c1fbde5e349cab82a1
Maja Kędras <r2d2ka@gmail.com>
2016-11-22 15:30:09 +0100

    Dodaje specjalne powitanie dla Bartusiów

--- javascript/welcome.js
+++ javascript/welcome.js
@@ -1,5 +1,8 @@
 var name;
 name = window.prompt("Podaj nam swoje imię");
+if(name == "Bartuś"){
+  window.alert("Redakcja Encyklopedii słodyczy serdecznie wita wszystkich Bartusiów <3");
+}
 function powitaj(){
   document.getElementById('welcome').innerHTML = "Witaj " + name + "! Słodkiego dnia <3";
 }
