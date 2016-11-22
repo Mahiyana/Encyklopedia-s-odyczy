e1e723a78968d0ee5cd05ee81541b90900a6c66d
Maja Kędras <r2d2ka@gmail.com>
2016-11-22 15:32:41 +0100

    Dodaje więcej form imienia Bartuś

--- javascript/welcome.js
+++ javascript/welcome.js
@@ -1,6 +1,6 @@
 var name;
 name = window.prompt("Podaj nam swoje imię");
-if(name == "Bartuś"){
+if(name == "Bartuś" || name == "Bartek" || name == "Bartosz" || name == "Bartłomiej" ){
   window.alert("Redakcja Encyklopedii słodyczy serdecznie wita wszystkich Bartusiów <3");
 }
 function powitaj(){
