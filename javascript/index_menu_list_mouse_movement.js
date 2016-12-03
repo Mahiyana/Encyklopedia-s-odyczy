6b39d36fadb223eae5cffc9583d594fbae5ceb85
Avarajihari <avara.jihari@kyr>
2016-12-04 00:38:32 +0100

    Dodaje mouseover i mouseout w index

---
+++ javascript/index_menu_list_mouse_movement.js
@@ -0,0 +1,8 @@
+function menu_mouseover()
+{
+	document.getElementById('index_info').innerHTML='W tym miejscu znajduje się lista słodyczy, jakie można znaleźć w każdym zakątku świata! Kliknij, aby dowiedzieć się więcej!';
+}
+function menu_mouseout()
+{
+	document.getElementById('index_info').innerHTML='Tutaj będzie encyklopedia słodyczy. I będzie ona przepyszna!';
+}
