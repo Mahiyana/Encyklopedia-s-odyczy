0a94e3dabe2af9d5c7efbf57656e54f6330fa2fd
Maja Kędras <r2d2ka@gmail.com>
2016-12-04 13:51:40 +0100

    Dodaje licznik anchorów w indeksie

--- javascript/licznik_linkow.js
+++ javascript/licznik_linkow.js
@@ -1,4 +1,20 @@
 function link_counter(){
   var num = document.links.length;
-  document.getElementById('link_count').innerHTML = "Czy wiesz, że na tej stronie możesz kliknąć w aż " + num + " linków?!";
+  var text;
+  if(num == 0){
+    text = "Niestety dziś nie ma w co klikać :< Przepraszamy ;_;";
+  }else{
+   text = "Czy wiesz, że na tej stronie możesz kliknąć w aż " + num + " linków?!";
+  }
+  document.getElementById('link_count').innerHTML = text;
+
+  var num2 = document.anchors.length;
+  var text2;
+  if(num2 == 0){
+    text2 = "A anchorów niestety nie ma :(";
+  }else{
+   text2 = "A do tego można kliknąć w " + num2 + " anchorów <3";
+  }
+  document.getElementById('anchor_count').innerHTML = text2;
+  
 }
