fdd5ec2a633b152d9d037b0c7df9f38e4561a00a
Maja Kędras <r2d2ka@gmail.com>
2016-12-04 13:45:13 +0100

    Dodaje licznik linków na stronie głównej

---
+++ javascript/licznik_linkow.js
@@ -0,0 +1,4 @@
+function link_counter(){
+  var num = document.links.length;
+  document.getElementById('link_count').innerHTML = "Czy wiesz, że na tej stronie możesz kliknąć w aż " + num + " linków?!";
+}
