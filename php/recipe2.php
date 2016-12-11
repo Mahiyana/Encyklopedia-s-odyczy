be0f00cfdde5b4edc28c5b1d1849292d919e12ab
Avarajihari <avarajihari@ar.ar>
2016-12-11 15:18:16 +0100

    Przenosze recipe2 do php

---
+++ php/recipe2.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+
+<html>
+
+	<head>
+		<meta charset = "utf-8">
+		<meta name = "author" content = "Maja & Róża">
+		<meta name = "description" content = "Przepis na babeczki z malinami">
+		<title>Waniliowe Naleśniki</title>
+		<link rel="stylesheet" type="text/css" href="../css/layout.css">
+		<link rel="stylesheet" type="text/css" href="../css/recipe.css">
+		<link href="https://fonts.googleapis.com/css?family=Ravi+Prakash" rel="stylesheet">
+	</head>
+
+	<body>
+	<div class='background'>
+	<div id='menu_header' class='absolute'>
+	<nav>
+	<p id='title'> Encyklopedia Słodyczy</p>
+		<table id='menu_header_table'>
+			<tr>
+				<td><a href = 'index.html'>Strona główna</a></td>
+				<td><a href = 'recipes.html'>Przepisy</a></td>
+				<td><a href = 'history.html'>Historia</a></td>
+			</tr>
+		</table>
+    </nav>
+	</div>
+	<div class="box">
+	<div id="menu_list">
+	<table id='menu_list_table'>
+		<tr>
+			<td><a href = 'recipe1.html'>Babeczki z malinami</a></td>
+		</tr>
+		<tr>
+			<td><a href = 'recipe2.html'>Naleśniki waniliowe</a></td>
+		</tr>
+		<tr>
+			<td>przepis3</td>
+		</tr>
+	</table>
+	</div>
+	<div class="contents_rhombus">
+    <article>
+		<h1>Waniliowe Naleśniki</h1>
+		<p>Trudność przepisu:
+		<meter min = "0"
+			max = "5"
+			value = "1">
+		</meter>
+		Łatwy!</p>
+		<br>
+		<section id ='ingredients_section'>
+			<h2>Składniki: </h2>
+			
+				<table id='ingredients'>
+					<tr>
+						<th>Nazwa Składnika</th>
+						<th>Potrzebna Ilość</th>
+					</tr>
+					<tr>
+						<td>mleko</td>
+						<td>1 szklanki</td>
+					</tr>
+					<tr>
+						<td>mąka pszenna</td>
+						<td>1 szklanka</td>
+					</tr>
+					<tr>
+						<td>mąka pszenna</td>
+						<td>2 szklanki</td>
+					</tr>
+					<tr>
+						<td>jajka</td>
+						<td>2 sztuki</td>
+					</tr>
+					<tr>
+						<td>cukier waniliowy</td>
+						<td>1 łyżka</td>
+					</tr>
+					<tr>
+						<td>sól</td>
+						<td>1 szczypta</td>
+					</tr>
+				</table>
+			
+		</section>
+		<br/><br/>
+		<section id = 'preparing'>
+			<h2>Przygotowanie</h2>
+			<div class='contents_box'>
+			<ol>
+				<li>Zmieszaj mąkę, mleko, jajka, sól i cukier w misce. Ubij mikserem.</li>
+				<li>Na rozgrzaną patelnię wylewaj po jednej chochli przygotowanej masy naleśnikowej.</li>
+				<li>Po usmażeniu każdego naleśnika z obu stron na jasno-złoty kolor, naleśniki są gotowe do spożycia! Najlepiej smakują na ciepło z dodatkiem takim jak nutella czy bita śmietana.</li>
+			</ol>
+			</div>
+		</section>
+		<br/><br/>
+
+		<div class='image'><img src='../pictures/pancake.jpg' class='image_op' alt='Zdjęcie do przepisu na naleśniki waniliowe'></div>
+    </article>
+	</div>
+	</div>
+	</div>
+	</body>
+
+</html>
+
