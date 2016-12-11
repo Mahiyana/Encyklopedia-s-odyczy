dcbf0f409b3c6f8ec052e746dd42074c90fc8c2b
Maja Kędras <r2d2ka@gmail.com>
2016-12-11 13:04:23 +0100

    Naprawia tamtego wrednego reseta

--- javascript/reset_form.js
+++ javascript/reset_form.js
@@ -2,9 +2,10 @@ function reset_form()
 {
 	document.getElementById("reset_button").addEventListener("click", reset_f);
 	
-	function reset_f()
+	function reset_f(e)
 	{
 	  document.getElementById("whole_form").reset();
+    e.preventDefault();
   }
 }
 
