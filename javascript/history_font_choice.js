898b0aad81d716b3dae14ae11a9ccef33d1885d0
Avarajihari <avara.jihari@ar.ar>
2016-12-15 10:58:02 +0100

    Zaczynam wprowadzac cookie w font

--- javascript/history_font_choice.js
+++ javascript/history_font_choice.js
@@ -3,6 +3,14 @@ function history_font_choice()
 	var bfont = document.getElementById('history_font_btn');
 	document.getElementById('times').checked = true;
 	bfont.onclick = ffont;
+	//nowe
+	var info = document.getElementById('history_info');
+	var radio1 = document.getElementById('ravi');
+	radio1.onclick = raviFont;
+	var radio2 = document.getElementById('arial');
+	radio2.onclick = arialFont;
+	var radio3 = document.getElementById('times');
+	radio3.onclick = timesFont;
 
 	function ffont()
 	{
@@ -22,5 +30,19 @@ function history_font_choice()
 		{
 			info.className = 'font_times';
 		}
+	}
+	
+	//nowe
+	function raviFont()
+	{
+		info.className = 'font_ravi';
+	}
+	function arialFont()
+	{
+		info.className = 'font_arial';
+	}
+	function timesFont()
+	{
+		info.className = 'font_times';
 	}
 }
